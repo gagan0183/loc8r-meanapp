90424d3abfa346c7ec9083e05c3821be6f7dd895
gagan0183 <g.deepsingh1@gmail.com>
2017-07-09 17:31:10 +0530

    added location-list page

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
-var ctrlMain = require('../controllers/main');
 var router = express.Router();
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
 
-/* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
